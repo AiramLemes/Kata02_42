e08fda30d8dbb16d7f523a90d4924624a3da17da
Airam <airam@10.194.2.11>
2021-09-30 10:24:07 +0100

    Tercera versión de Kata 2.
    Se añade una clase histogram para crear un histograma (Hashmap).

---
+++ src/main/java/kata02_42/Histogram.java
@@ -0,0 +1,44 @@
+
+package kata02_42;
+
+import java.util.HashMap;
+
+
+public class Histogram {
+    
+    
+    private final int[] data;
+
+    
+    
+    public Histogram(int[] data) {
+        
+        this.data = data;
+        
+    }
+    
+    
+    
+    public int[] getData() {
+        return data;
+    }
+    
+    
+    
+    
+    public HashMap<Integer, Integer> getHistogram() {
+        HashMap<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+        
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+        }
+         
+        return histogram;
+    }
+
+   
+    
+    
+    
+    
+}
